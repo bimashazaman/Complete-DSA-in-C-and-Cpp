e31ac0239d13e019427503ee106adf10632ab849
bimashazaman <bimashazaman1234@gmail.com>
2022-10-18 23:42:27 +0600

    Ref

---
+++ reference.cpp
@@ -0,0 +1,21 @@
+
+#include <stdio.h>
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+
+
+
+
+
+int main()
+{
+ 
+    int a = 10;
+    int &r = a;
+
+    cout << a << endl;
+      
+  
+    return 0;
+}
