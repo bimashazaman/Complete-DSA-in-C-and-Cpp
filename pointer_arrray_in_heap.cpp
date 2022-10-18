3c91ba111863a0f317c522101090d1ce94219397
bimashazaman <bimashazaman1234@gmail.com>
2022-10-18 22:42:53 +0600

    Pointer in the heap

---
+++ pointer_arrray_in_heap.cpp
@@ -0,0 +1,27 @@
+
+#include <stdio.h>
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+
+
+int main()
+{
+ 
+
+   int *POINER; // pointer to an integer
+   
+   POINER = (int *)malloc(5 * sizeof(int)); // allocate memory for 5 integers using malloc IN HEAP
+   
+   POINER[0]= 10; // assign value to first element
+   POINER[1]= 12; // assign value to second element
+   POINER[2] = 323; // assign value to third element
+   
+  
+   
+   for(int i=0; i<5; i++) // print values of array
+       cout << POINER[i] << endl;
+   
+
+    return 0;
+}
